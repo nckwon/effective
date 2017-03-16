c0b8913a0f3633dbc7670fe7c2cdc23ae0e33cf3
Nam,ChangKwon <nckwon@nate.com>
2017-03-17 06:35:58 +0900

    ..

---
+++ src/main/java/com/effective/ch16/InstrumentedHashSet.java
@@ -0,0 +1,41 @@
+package com.effective.ch16;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Created by nckwon on 2017. 3. 17..
+ */
+public class InstrumentedHashSet<E> extends HashSet<E> {
+
+    private int addCount = 0;
+
+    public InstrumentedHashSet() {
+
+    }
+
+    public InstrumentedHashSet(int initCap, float loadFactor) {
+        super(initCap, loadFactor);
+    }
+
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+    public int getAddCount() {
+        return addCount;
+    }
+
+    public static void main(String[] args) {
+        InstrumentedHashSet<String> s = new InstrumentedHashSet<String>();
+        s.addAll(Arrays.asList("Snap", "Crackle", "Pop"));
+        System.out.println("size:" + s.size());
+        System.out.println("addCount:" + s.getAddCount());
+    }
+}
